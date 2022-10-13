e88f5a985fc2f3201eb534775ef2ba4468c7c927
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2022-10-13 17:52:51 +0630

    fourth commit

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const msg = document.querySelector("#msg")
 send.onclick = () => {
     msg.style.opacity = "1";
 }
+
+const nav = document.querySelector("#navbar")
+const toggle = document.querySelector(".toggle")
+const nav_toggle = document.querySelector("#toggle")
+
+nav.onclick = () => {
+    toggle.style.backgroundImage = "none";
+}
+// toggle.style.backgroudImage = "none";
+// nav_toggle.style.transform = "translateX(0)";
