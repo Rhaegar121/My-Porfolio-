f745807c4cc85515dcf78d041142e14c664078ba
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2022-10-31 19:38:33 +0630

    fifth commit

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// dark-theme
+const icon = document.querySelector("#icon")
+
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme")
+    if(document.body.classList.contains("dark-theme")) {
+      icon.classList.remove("fa-moon")
+      icon.classList.add("fa-sun")
+    } else {
+      icon.classList.add("fa-moon")
+      icon.classList.remove("fa-sun")
+    }
+}
+
 const send = document.querySelector("#send")
 const msg = document.querySelector("#msg")
 
